912c96ff1d2c86e536172c98496182ae782466ca
0xMenna01 <emanuele.cryptdev@gmail.com>
2023-07-04 20:42:04 +0200

    tests iterations for JaCoCo and Pit, includind new rules and testing other default generators

--- core/spring/src/test/java/org/apache/syncope/core/spring/security/PasswordGeneratorTest.java
+++ core/spring/src/test/java/org/apache/syncope/core/spring/security/PasswordGeneratorTest.java
@@ -10,6 +10,7 @@ import org.apache.syncope.core.spring.SpringTestConfiguration;
 import org.apache.syncope.core.spring.security.utils.TestUtils;
 import org.apache.syncope.core.spring.security.utils.impl.TestImplementation;
 import org.apache.syncope.core.spring.security.utils.impl.TestPasswordPolicy;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -31,7 +32,6 @@ public class PasswordGeneratorTest {
         boolean isExceptionExpected = TestUtils.isExceptionExpected(policiesType);
 
         try {
-
             List<PasswordPolicy> policies = null;
             switch (policiesType) {
                 case EMPTY_LIST:
@@ -64,11 +64,99 @@ public class PasswordGeneratorTest {
         }
     }
 
+
+    @ParameterizedTest
+    @MethodSource("buildRules")
+    public void testGenerationMultipleRules(GenerationRule generationRule) {
+        DefaultPasswordRuleConf defaultPasswordRuleConf = TestUtils.createDefaultPasswordRuleConfig();
+        testMultipleRules(defaultPasswordRuleConf, generationRule);
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("buildRules")
+    public void testOtherGeneratorsMultipleRules(GenerationRule generationRule) {
+        DefaultPasswordRuleConf[] defaultGenerators = TestUtils.buildOtherGenerators();
+        for (DefaultPasswordRuleConf generator : defaultGenerators) {
+            testMultipleRules(generator, generationRule);
+        }
+    }
+
+    @Test
+    public void testFixedDigitsPasswd() {
+        DefaultPasswordRuleConf onlyDigitGenerator = TestUtils.createLowMaxLengthOnlyDigitConfig();
+        TestImplementation passwordRule = new TestImplementation();
+        passwordRule.setBody(POJOHelper.serialize(onlyDigitGenerator));
+        List<PasswordPolicy> policies = List.of(new TestPasswordPolicy(passwordRule));
+
+        String generatedPassword = passwordGenerator.generate(policies);
+        assertTrue(generatedPassword.chars().allMatch(Character::isDigit));
+    }
+
+    @Test
+    public void testFixedAlphabeticalPasswd() {
+        DefaultPasswordRuleConf onlyAlphabeticalConfig = TestUtils.createLowMaxLengthOnlyAlphabeticalConfig();
+        TestImplementation passwordRule = new TestImplementation();
+        passwordRule.setBody(POJOHelper.serialize(onlyAlphabeticalConfig));
+        List<PasswordPolicy> policies = List.of(new TestPasswordPolicy(passwordRule));
+
+        String generatedPassword = passwordGenerator.generate(policies);
+        assertTrue(generatedPassword.chars().allMatch(Character::isAlphabetic));
+    }
+
+
+    private void testMultipleRules(DefaultPasswordRuleConf defaultPasswordRuleConf, GenerationRule generationRule) {
+        setGenerationRule(defaultPasswordRuleConf, generationRule);
+
+        TestImplementation passwordRule = new TestImplementation();
+        passwordRule.setBody(POJOHelper.serialize(defaultPasswordRuleConf));
+        List<PasswordPolicy> policies = List.of(new TestPasswordPolicy(passwordRule));
+
+        String generatedPassword = passwordGenerator.generate(policies);
+        assertRule(generatedPassword, generationRule);
+    }
+
+    private void assertRule(String generatedPassword, GenerationRule generationRule) {
+        switch (generationRule) {
+            case ALPHABETICAL -> assertTrue(generatedPassword.chars().anyMatch(Character::isAlphabetic));
+            case UPPERCASE -> assertTrue(generatedPassword.chars().anyMatch(Character::isUpperCase));
+            case LOWERCASE -> assertTrue(generatedPassword.chars().anyMatch(Character::isLowerCase));
+            case DIGIT -> assertTrue(generatedPassword.chars().anyMatch(Character::isDigit));
+            case SPECIAL ->
+                assertTrue(generatedPassword.chars().anyMatch(c -> '$' == c || '!' == c || '#' == c || '*' == c));
+        }
+    }
+
+
+    private void setGenerationRule(DefaultPasswordRuleConf defaultPasswordRuleConf, GenerationRule generationRule) {
+        switch (generationRule) {
+            case ALPHABETICAL -> defaultPasswordRuleConf.setAlphabetical(1);
+            case UPPERCASE -> defaultPasswordRuleConf.setUppercase(1);
+            case LOWERCASE -> defaultPasswordRuleConf.setLowercase(1);
+            case DIGIT -> defaultPasswordRuleConf.setDigit(1);
+            case SPECIAL -> {
+                defaultPasswordRuleConf.setSpecial(1);
+                defaultPasswordRuleConf.getSpecialChars().add('$');
+                defaultPasswordRuleConf.getSpecialChars().add('!');
+                defaultPasswordRuleConf.getSpecialChars().add('#');
+                defaultPasswordRuleConf.getSpecialChars().add('*');
+            }
+        }
+    }
+
     public enum PoliciesType {
         VALID_LIST,
         INVALID_LIST,
         EMPTY_LIST,
         NULL,
+    }
+
+    private enum GenerationRule {
+        ALPHABETICAL,
+        UPPERCASE,
+        LOWERCASE,
+        DIGIT,
+        SPECIAL,
     }
 
 
@@ -78,7 +166,17 @@ public class PasswordGeneratorTest {
         for (PoliciesType value : PoliciesType.values()) {
             Arguments argument = Arguments.of(value);
             parameters.add(argument);
+        }
+        return parameters.stream();
+    }
 
+
+    private static Stream<Arguments> buildRules() {
+        List<Arguments> parameters = new ArrayList<>();
+
+        for (GenerationRule rule : GenerationRule.values()) {
+            Arguments argument = Arguments.of(rule);
+            parameters.add(argument);
         }
         return parameters.stream();
     }
--- core/spring/src/test/java/org/apache/syncope/core/spring/security/utils/TestUtils.java
+++ core/spring/src/test/java/org/apache/syncope/core/spring/security/utils/TestUtils.java
@@ -31,6 +31,49 @@ public class TestUtils {
         return defaultPasswordRuleConf;
     }
 
+    private static DefaultPasswordRuleConf createEmptyPasswordRuleConfig() {
+        DefaultPasswordRuleConf defaultPasswordRuleConf = new DefaultPasswordRuleConf();
+        return defaultPasswordRuleConf;
+    }
+
+    public static DefaultPasswordRuleConf createLowMaxLengthOnlyDigitConfig() {
+        DefaultPasswordRuleConf defaultPasswordRuleConf = new DefaultPasswordRuleConf();
+        defaultPasswordRuleConf.setMinLength(5);
+        defaultPasswordRuleConf.setMaxLength(5);
+        defaultPasswordRuleConf.setRepeatSame(2);
+        defaultPasswordRuleConf.setDigit(5);
+        return defaultPasswordRuleConf;
+    }
+
+    public static DefaultPasswordRuleConf createLowMaxLengthOnlyAlphabeticalConfig() {
+        DefaultPasswordRuleConf defaultPasswordRuleConf = new DefaultPasswordRuleConf();
+        defaultPasswordRuleConf.setMinLength(5);
+        defaultPasswordRuleConf.setMaxLength(5);
+        defaultPasswordRuleConf.setAlphabetical(5);
+        return defaultPasswordRuleConf;
+    }
+
+    private static DefaultPasswordRuleConf createZeroLengthPasswdRule() {
+        DefaultPasswordRuleConf defaultPasswordRuleConf = new DefaultPasswordRuleConf();
+        defaultPasswordRuleConf.setMinLength(0);
+        return defaultPasswordRuleConf;
+    }
+
+    private static DefaultPasswordRuleConf createMinGreaterThanMaxLengthRule() {
+        DefaultPasswordRuleConf defaultPasswordRuleConf = new DefaultPasswordRuleConf();
+        defaultPasswordRuleConf.setMinLength(2);
+        defaultPasswordRuleConf.setMaxLength(1);
+        return defaultPasswordRuleConf;
+    }
+
+    public static DefaultPasswordRuleConf[] buildOtherGenerators() {
+        DefaultPasswordRuleConf emptyRulesGenerator = createEmptyPasswordRuleConfig();
+        DefaultPasswordRuleConf minLengthZeroGenerator = createZeroLengthPasswdRule();
+        DefaultPasswordRuleConf minGreaterThanMaxGenerator = createMinGreaterThanMaxLengthRule();
+
+        return new DefaultPasswordRuleConf[]{emptyRulesGenerator, minLengthZeroGenerator, minGreaterThanMaxGenerator};
+    }
+
 
     public static boolean isExceptionExpected(PasswordGeneratorTest.PoliciesType policiesType) {
         boolean isExceptionExpected = true;
